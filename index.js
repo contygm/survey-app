b891532852e6a39f666c2c32c866016de3f1c696
Genevieve Conty <genevieve.conty@AV-MBP-7208.hitronhub.home>
2018-03-31 16:31:53 -0700

    set up google auth

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ EMPIRE : 'DID NOTHING WRONG' });
-});
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('accessToken', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
+  })
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+// XXX: test route
+app.get(
+  '/',
+  (req, res) => {
+    res.send({ hi: 'there '});
+  }
+);
 
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, function () {
-  console.log('server is live');
+  console.log('IT\'S ALIVE');
 });
