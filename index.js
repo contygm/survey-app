7743908f052c7094bb35bb7c5b8d4133e2a50dc6
Genevieve Conty <genevieve.conty@AV-MBP-7208.hitronhub.home>
2018-03-31 15:16:25 -0700

    changing salutations

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ EMPIRE : 'DID NOTHING WRONG' });
 });
 
 const PORT = process.env.PORT || 4200;
