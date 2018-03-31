40c72d53488a7a2f927a155115941cb17b3b3892
Genevieve Conty <genevieve.conty@AV-MBP-7208.hitronhub.home>
2018-03-31 14:46:25 -0700

    setting up basic heroku deployment

--- server/index.js
+++ server/index.js
@@ -6,4 +6,7 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 4200;
+app.listen(PORT, function () {
+  console.log('server is live');
+});
